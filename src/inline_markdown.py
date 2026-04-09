6867a51a34d72194ee1419ede6e8250798c138b0
DaMaGi <35659412+DaMaGi@users.noreply.github.com>
2026-04-09 10:15:55 -0300

    create img and link extraction files

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -22,29 +22,31 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         new_nodes.extend(split_nodes)
     return new_nodes
 
-def split_nodes_delimiter(old_nodes, delimiter, text_type):
-    new_nodes = []
-    for old_node in old_nodes:
-        if old_node.text_type != TextType.TEXT:
-            new_nodes.append(old_node)
-            continue
-        active_styles = {TextType.BOLD: False, TextType.ITALIC: False, TextType.CODE: False}
-        split_nodes = []
-        sections = re.split(r"**|_|`", old_node.text)
-        for sect in sections:
-            if sect == "":
-                continue
-            match sect:
-                case '**':
-                    active_styles ^= {TextType.BOLD}
-                case '_':
-                    active_styles ^= {TextType.ITALIC}
-                case '`':
-                    active_styles ^= {TextType.CODE}
-                case _:
-                    split_nodes.append(TextNode(sect, {k for k, v in active_styles if v is True}))
-        new_nodes.extend(split_nodes)
-    return new_nodes
+
+# TODO: Include better pattern recognitions for each style and add single '*' to italics. Styles should have a /w character next to them.
+# def split_nodes_delimiter(old_nodes, delimiter, text_type):
+#     new_nodes = []
+#     for old_node in old_nodes:
+#         if old_node.text_type != TextType.TEXT:
+#             new_nodes.append(old_node)
+#             continue
+#         active_styles = {TextType.BOLD: False, TextType.ITALIC: False, TextType.CODE: False}
+#         split_nodes = []
+#         sections = re.split(r"**|_|`", old_node.text)
+#         for sect in sections:
+#             if sect == "":
+#                 continue
+#             match sect:
+#                 case '**':
+#                     active_styles ^= {TextType.BOLD}
+#                 case '_':
+#                     active_styles ^= {TextType.ITALIC}
+#                 case '`':
+#                     active_styles ^= {TextType.CODE}
+#                 case _:
+#                     split_nodes.append(TextNode(sect, {k for k, v in active_styles if v is True}))
+#         new_nodes.extend(split_nodes)
+#     return new_nodes
 
 
     
